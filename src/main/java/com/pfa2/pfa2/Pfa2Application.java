14d717e422211dbe12e9a2fc1a504159a8013e51
Safa <safahmad02@gmail.com>
2023-03-24 16:32:39 +0100

    Initial commit

---
+++ src/main/java/com/pfa2/pfa2/Pfa2Application.java
@@ -0,0 +1,13 @@
+package com.pfa2.pfa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pfa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pfa2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfa2/pfa2/Pfa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa2.pfa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pfa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
